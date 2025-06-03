ce100c68013fb237f52af971c246b8830932bbac
almat101 <sandruozzo@gmail.com>
2025-06-03 16:12:16 +0000

    Use of 2862410 in healthchecks, add .env for proper signup

--- backend/auth_service/start.sh
+++ backend/auth_service/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python manage.py crontab remove
 python manage.py crontab add
-python manage.py makemigrations
+#python manage.py makemigrations
 python manage.py migrate
 gunicorn auth_service.wsgi:application --bind 0.0.0.0:8001 --workers=3
--- backend/history/start.sh
+++ backend/history/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-python manage.py makemigrations
+#python manage.py makemigrations
 python manage.py migrate
-gunicorn history_service.wsgi:application --bind 0.0.0.0:8002 --workers=3
-      
+gunicorn history_service.wsgi:application --bind 0.0.0.0:8002 --workers=3 
