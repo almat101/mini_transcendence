16fce9a6ca917258093a8437fe6aaa75d4c3dd2b
almat101 <sandruozzo@gmail.com>
2025-09-20 00:12:17 +0200

    test(auth_app): add 2 integration tests for login and test CI pipeline
    - Added two integration tests for the login endpoint in auth_app
    - Updated GitHub Actions workflow to run integration tests via Docker Compose in the CI stage

--- backend/auth_service/django/auth_app/tests.py
+++ backend/auth_service/django/auth_app/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class AuthAppLoginTest(APITestCase):
+    def test_login_empty_field(self):
+        response = self.client.post('/api/auth/login/', {}) 
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('error', response.data)
+
+    def test_login_wrong_credentials(self):
+            data = {
+                "username": "wrong_user",
+                "password": "worngp_asswords"
+            }
+            response = self.client.post('/api/auth/login/', data)
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('error', response.data)
